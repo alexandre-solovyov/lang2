87f738faf40e262914bba6d37f7136606edc5a01
alex <alex>
2018-02-19 17:29:57 +0300

    new french text

--- analysis/Main.cpp
+++ analysis/Main.cpp
@@ -3,18 +3,12 @@
 #include <model/Tools.h>
 #include <QDir>
 
-#ifdef WIN32
-    const QString LANG_FOLDER = "D:/asl/lang3/lang/progress/french";
-    const QString TEXTS_FOLDER = "D:/asl/lang3/lang/texts/french";
-#else
-    const QString LANG_FOLDER = QDir::home().absoluteFilePath("lang2/progress/french");
-    const QString TEXTS_FOLDER = QDir::home().absoluteFilePath("lang2/texts/french");
-#endif
+QString LANG_FOLDER;
+QString TEXTS_FOLDER;
+QString LANG;
 
-const QString LANG = "fr";
-
-bool IS_VERBOSE = false;
-const int  LIST_TRIM = 70;
+bool    IS_VERBOSE = false;
+int     LIST_TRIM = 70;
 
 StdModel MODEL;
 
@@ -75,7 +69,7 @@ bool CheckText( const QString& thePath )
     {
         QMap<QString, int>::const_iterator it = unknown.begin(), last = unknown.end();
         for( ; it!=last; it++ )
-            Tools::print( QString( "%0: %1" ).arg(it.key()).arg(it.value()) );
+            Tools::print( QString( "%0: %1" ).arg(it.key()).arg(it.value()) + ";  ", false );
     }
     else
     {
@@ -105,13 +99,40 @@ void CheckTexts( int theIndex, bool isSeveral )
         CheckText( files[theIndex-1].absoluteFilePath() );
 }
 
+#define FRANCAIS
+//#define GERMAN
+
 int main( int /*argc*/, char** /*argv*/ )
 {
-    IS_VERBOSE = false;
+#ifdef FRANCAIS
+    IS_VERBOSE = true;
+    #ifdef WIN32
+        LANG_FOLDER = "D:/asl/lang3/lang/progress/french";
+        TEXTS_FOLDER = "D:/asl/lang3/lang/texts/french";
+    #else
+        LANG_FOLDER = QDir::home().absoluteFilePath("lang2/progress/french");
+        TEXTS_FOLDER = QDir::home().absoluteFilePath("lang2/texts/french");
+    #endif
+    LANG = "fr";
+#endif
+#ifdef GERMAN
+    IS_VERBOSE = true;
+    #ifdef WIN32
+        LANG_FOLDER = "D:/asl/lang3/lang/progress/german";
+        TEXTS_FOLDER = "D:/asl/lang3/lang/texts/german";
+    #else
+        LANG_FOLDER = QDir::home().absoluteFilePath("lang2/progress/german");
+        TEXTS_FOLDER = QDir::home().absoluteFilePath("lang2/texts/german");
+    #endif
+    LANG = "de";
+#endif
+
     Load();
 
-    CheckTexts( 3, true );
+    //CheckTexts( 1, false );
+    //CheckTexts( 3, true );
     //CheckTexts( 4, false );
+    CheckTexts( 6, false );
 
     return 0;
 }
